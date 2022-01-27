696c14c8feed0bbcc023783567b8117d5dd6597d
Alec Keeler <alec.keeler@gmail.com>
2022-01-27 15:16:03 -0600

    w10d4complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { User, Post, sequelize, Sequelize: { Op } } = require('./models');
+const { User, Post, Subbreaddit, sequelize, Sequelize: { Op } } = require('./models');
 
 async function buildUser() {
     const user = User.build({
@@ -137,4 +137,43 @@ async function testOrder() {
     sequelize.close()
 }
 
-testOrder()
+// testOrder()
+
+async function profile(userId) {
+    const user = await User.findByPk(userId, {
+        include: Post
+    })
+
+    console.log(user.Posts[0].content)
+
+    sequelize.close()
+}
+
+// profile(3)
+
+async function getPostAndStuff(postId) {
+    const post = await Post.findByPk(postId, {
+        include: [User, Subbreaddit]
+    })
+
+    console.log(post.User.username, post.Subbreaddit.name)
+
+    sequelize.close()
+}
+
+// getPostAndStuff(1)
+
+async function profilePlus(userId) {
+    const user = await User.findByPk(userId, {
+        include: {
+            model: Post,
+            include: Subbreaddit
+        }
+    })
+
+    console.log(user.Posts[0].Subbreaddit.name)
+
+    sequelize.close()
+}
+
+profilePlus(3)
