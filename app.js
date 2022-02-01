424af4e306a65986f5d2d790afbd55bf236f6284
Alec Keeler <alec.keeler@gmail.com>
2022-02-01 12:56:44 -0600

    task 6

--- app.js
+++ app.js
@@ -7,32 +7,15 @@ const app = express();
 // Task 3a
 app.set('view engine', 'pug')
 
-// Task 2
-app.get('/', async (req, res) => {
-    // res.send('Welcome to Breaddit')
+// Task 6b
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+app.use('/profile', usersRouter)
 
-    // Task 3b
-    // res.render('users', {num: 1000000})
-
-    // Task 5
-    const users = await User.findAll();
-
-    res.render('users', {users})
+// Task 7
+app.get(/^\/(home)[st]?$/, (req, res) => {
+    res.send('Regex handler')
 })
-
-// Task 4
-app.get('/users/:userId', async (req, res) => {
-    // console.log(req.params.userId)
-    const userId = req.params.userId
-    const user = await User.findByPk(userId)
-
-    // console.log(user)
-
-    res.render('profile', {user})
-})
-
-
-
 
 // Task 1b
 const port = 8080;
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+// Task 6a
+
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models');
+
+// Task 2
+router.get('/', async (req, res) => {
+    // res.send('Welcome to Breaddit')
+
+    // Task 3b
+    // res.render('users', {num: 1000000})
+
+    // Task 5
+    const users = await User.findAll();
+
+    res.render('users', { users })
+})
+
+// Task 4
+router.get('/:userId', async (req, res) => {
+    // console.log(req.params.userId)
+    const userId = req.params.userId
+    const user = await User.findByPk(userId)
+
+    // console.log(user)
+
+    res.render('profile', { user })
+})
+
+//Task 6b
+module.exports = router;
