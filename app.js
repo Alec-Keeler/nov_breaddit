eba83ad7130a1060d8a22d832a382dc4d98de467
Alec Keeler <alec.keeler@gmail.com>
2022-02-01 12:03:25 -0600

    task 4

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Task 1a
 const express = require('express');
+const { User } = require('./models');
 
 const app = express();
 
@@ -12,6 +13,17 @@ app.get('/', (req, res) => {
 
     // Task 3b
     res.render('users', {num: 1000000})
+})
+
+// Task 4
+app.get('/users/:userId', async (req, res) => {
+    // console.log(req.params.userId)
+    const userId = req.params.userId
+    const user = await User.findByPk(userId)
+
+    // console.log(user)
+
+    res.render('profile', {user})
 })
 
 
