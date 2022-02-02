4d767b0b77df65fa7a51d5c61738474bfe679df7
Alec Keeler <alec.keeler@gmail.com>
2022-02-02 12:43:36 -0600

    task 12

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ app.set('view engine', 'pug')
 // Task 8
 app.use(express.static('./public'))
 
+// Task 12a
+app.use((req, res, next) => {
+    req.banana = true
+    next()
+})
+
 // Task 6b
 const usersRouter = require('./routes/users')
 app.use('/users', usersRouter)
-app.use('/profile', usersRouter)
+// app.use('/profile', usersRouter)
 
 // Task 7
 app.get(/^\/(home)[st]?$/, (req, res) => {
+    console.log('banana?', req.banana)
+    console.log('potato?', req.potato)
     res.send('Regex handler')
 })
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,25 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
 
+// Task 12b
+router.use((req, res, next) => {
+    console.log('we have arrived at the users router')
+    req.potato = true
+    next()
+})
+
+// Task 12c
+const emailChecker = (req, res, next) => {
+    // const exp = /^[\w\-]+@[\w]+\.([a-zA-Z]{2,3})$/;
+    // if (exp.test(req.body.email)) {
+    //     next()
+    // } else {
+    //     req.errors = ['Email is invalid']
+    //     next()
+    // }
+    console.log('hello from route specific middleware')
+}
+
 // Task 2
 router.get('/', async (req, res) => {
     // res.send('Welcome to Breaddit')
@@ -13,6 +32,7 @@ router.get('/', async (req, res) => {
 
     // Task 5
     const users = await User.findAll();
+    console.log('banana?', req.banana)
 
     res.render('users', { users })
 })
@@ -36,7 +56,7 @@ router.get('/signup', (req, res) => {
 })
 
 // Task 11c
-router.post('/signup', (req, res) => {
+router.post('/signup', emailChecker, (req, res) => {
     console.log('hello from post route')
 })
 
